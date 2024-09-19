6727c9e7bb1bc84805cf94929a130bedc4bf3645
Ashik11898 <mohamed.ashik@broadbil.com>
2024-09-19 16:41:15 +0400

    changes2

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <div>Mohamed ashik</div>
+        <div>Mohamed Ashik Owner of this site</div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
