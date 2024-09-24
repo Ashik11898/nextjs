02cb8e3a15f23d6243d8ba79c44d38829a84715e
Ashik11898 <mohamed.ashik@broadbil.com>
2024-09-24 13:04:26 +0400

    second  commit 2

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Save and see your changes instantly</li>
         </ol>
 
        
